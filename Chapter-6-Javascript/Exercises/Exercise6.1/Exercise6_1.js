982682b82f084a3c346b10f8b431e35ff60cb215
Shuaaa-j <JoshuaGofulco246@gmail.com>
2024-04-12 17:45:30 +0400

    Exercise6_1.css

--- Chapter-6-Javascript/Exercises/Exercise6.1/Exercise6_1.js
+++ Chapter-6-Javascript/Exercises/Exercise6.1/Exercise6_1.js
@@ -2,16 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const calculateBtn = document.getElementById('calculateBtn');
     const petrolCostInput = document.getElementById('petrolCost');
     const litersInput = document.getElementById('liters');
-    const totalCostDisplay = document.getElementById('totalCost');
+    const billsInput = document.getElementById('bills');
+    const receipt = document.getElementById('receipt');
     const resultDisplay = document.getElementById('result');
+    const billsInsertedDisplay = document.getElementById('billsInserted');
+    const changeDisplay = document.getElementById('change');
 
     calculateBtn.addEventListener('click', function() {
         const petrolCost = parseFloat(petrolCostInput.value);
         const liters = parseFloat(litersInput.value);
         const totalCost = petrolCost * liters;
+        const billsInserted = parseFloat(billsInput.value);
+        const change = billsInserted - totalCost;
 
-        resultDisplay.textContent = `AED${totalCost.toFixed(2)}`;
+        resultDisplay.textContent = `AED ${totalCost.toFixed(2)}`;
+        billsInsertedDisplay.textContent = `AED ${billsInserted.toFixed(2)}`;
+        changeDisplay.textContent = `AED ${change.toFixed(2)}`;
 
-        totalCostDisplay.classList.remove('hidden');
+        receipt.classList.remove('hidden');
     });
 });
