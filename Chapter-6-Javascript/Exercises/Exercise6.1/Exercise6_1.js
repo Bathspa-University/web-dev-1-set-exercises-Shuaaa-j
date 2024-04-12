688fd8905ae20eeebdc7d39396ce6db0ef7f37da
Shuaaa-j <JoshuaGofulco246@gmail.com>
2024-04-12 17:41:07 +0400

    Exercise6_1.js

--- Chapter-6-Javascript/Exercises/Exercise6.1/Exercise6_1.js
+++ Chapter-6-Javascript/Exercises/Exercise6.1/Exercise6_1.js
@@ -1,2 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const calculateBtn = document.getElementById('calculateBtn');
+    const petrolCostInput = document.getElementById('petrolCost');
+    const litersInput = document.getElementById('liters');
+    const totalCostDisplay = document.getElementById('totalCost');
+    const resultDisplay = document.getElementById('result');
+
+    calculateBtn.addEventListener('click', function() {
+        const petrolCost = parseFloat(petrolCostInput.value);
+        const liters = parseFloat(litersInput.value);
+        const totalCost = petrolCost * liters;
+
+        resultDisplay.textContent = `AED${totalCost.toFixed(2)}`;
+
+        totalCostDisplay.classList.remove('hidden');
+    });
 });
